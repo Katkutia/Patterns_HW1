747db752edf82d48949fd0e298fe27f579586316
Katerina Platonova <platonovihome@gmail.com>
2024-05-08 20:59:21 +0300

    first commit

---
+++ src/test/java/ru/netology/patterns/test/DeliveryTest.java
@@ -0,0 +1,195 @@
+package ru.netology.patterns.test;
+package ru.netology.delivery.test;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.Keys;
+import ru.netology.delivery.data.DataGenerator;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.open;
+
+class DeliveryTest {
+
+    @BeforeAll
+    static void setUpAll() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+
+    @BeforeEach
+    void setup() {
+        open("http://localhost:9999");
+    }
+
+    @AfterAll
+    static void tearDownAll() {
+        SelenideLogger.removeListener("allure");
+    }
+
+    @Test
+    @DisplayName("Should successful plan and replan meeting")
+    void shouldSuccessfulPlanAndReplanMeeting() {
+        var validUser = DataGenerator.Registration.generateUser("ru");
+        var daysToAddForFirstMeeting = 4;
+        var firstMeetingDate = DataGenerator.generateDate(daysToAddForFirstMeeting);
+        var daysToAddForSecondMeeting = 7;
+        var secondMeetingDate = DataGenerator.generateDate(daysToAddForSecondMeeting);
+        $("[data-test-id='city'] input").val(validUser.getCity());
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
+        $("[data-test-id='date'] input").val(firstMeetingDate);
+        $("[data-test-id='name'] input").val(validUser.getName());
+        $("[data-test-id='phone'] input").val(validUser.getPhone());
+        $("[data-test-id='agreement']").click();
+        $(".button").click();
+        $("[data-test-id='success-notification'] .notification__title").shouldBe(visible).shouldBe(exactText("Успешно!"));
+        $("[data-test-id='success-notification'] .notification__content").shouldBe(visible).shouldBe(exactText("Встреча успешно запланирована на " + firstMeetingDate));
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
+        $("[data-test-id='date'] input").val(secondMeetingDate);
+        $(".button").click();
+        $("[data-test-id='replan-notification'] .notification__title").shouldBe(visible).shouldBe(exactText("Необходимо подтверждение"));
+        $("[data-test-id='replan-notification'] .notification__content").shouldBe(visible).shouldBe(exactText("У вас уже запланирована встреча на другую дату. Перепланировать? Перепланировать"));
+        $("[data-test-id='replan-notification'] .button").click();
+        $("[data-test-id='success-notification'] .notification__title").shouldBe(visible).shouldBe(exactText("Успешно!"));
+        $("[data-test-id='success-notification'] .notification__content").shouldBe(visible).shouldBe(exactText("Встреча успешно запланирована на " + secondMeetingDate));
+    }
+
+    @Test
+    @DisplayName("Should stop because of city name")
+    void shouldNotSendFormWithIncorrectCity() {
+        var validUser = DataGenerator.Registration.generateUser("ru");
+        var daysToAddForFirstMeeting = 4;
+        var firstMeetingDate = DataGenerator.generateDate(daysToAddForFirstMeeting);
+        var daysToAddForSecondMeeting = 7;
+        var secondMeetingDate = DataGenerator.generateDate(daysToAddForSecondMeeting);
+        $("[data-test-id='city'] input").setValue("Orel");
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
+        $("[data-test-id='date'] input").val(firstMeetingDate);
+        $("[data-test-id='name'] input").val(validUser.getName());
+        $("[data-test-id='phone'] input").val(validUser.getPhone());
+        $("[data-test-id='agreement']").click();
+        $(".button").click();
+        $("[data-test-id='city'] .input__inner .input__sub")
+                .shouldBe(visible)
+                .shouldHave(text("Доставка в выбранный город недоступна"));
+    }
+
+    @Test
+    @DisplayName("Should stop because of out of city")
+    void shouldNotSendFormWithoutCity() {
+        var validUser = DataGenerator.Registration.generateUser("ru");
+        var daysToAddForFirstMeeting = 4;
+        var firstMeetingDate = DataGenerator.generateDate(daysToAddForFirstMeeting);
+        var daysToAddForSecondMeeting = 7;
+        var secondMeetingDate = DataGenerator.generateDate(daysToAddForSecondMeeting);
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
+        $("[data-test-id='date'] input").val(firstMeetingDate);
+        $("[data-test-id='name'] input").val(validUser.getName());
+        $("[data-test-id='phone'] input").val(validUser.getPhone());
+        $("[data-test-id='agreement']").click();
+        $(".button").click();
+        $("[data-test-id='city'] .input__inner .input__sub")
+                .shouldBe(visible)
+                .shouldHave(text("Поле обязательно для заполнения"));
+    }
+
+    @Test
+    @DisplayName("Should stop because of out of date")
+    void shouldNotSendFormWithoutDate() {
+        var validUser = DataGenerator.Registration.generateUser("ru");
+        var daysToAddForFirstMeeting = 4;
+        var firstMeetingDate = DataGenerator.generateDate(daysToAddForFirstMeeting);
+        var daysToAddForSecondMeeting = 7;
+        var secondMeetingDate = DataGenerator.generateDate(daysToAddForSecondMeeting);
+        $("[data-test-id='city'] input").val(validUser.getCity());
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
+        $("[data-test-id='name'] input").val(validUser.getName());
+        $("[data-test-id='phone'] input").val(validUser.getPhone());
+        $("[data-test-id='agreement']").click();
+        $(".button").click();
+        $("[data-test-id='date'] .input__inner .input__sub")
+                .shouldBe(visible)
+                .shouldHave(text("Неверно введена дата"));
+    }
+
+    @Test
+    @DisplayName("Should stop because of incorrect date")
+    void shouldNotSendFormWithIncorrectDate() {
+        var validUser = DataGenerator.Registration.generateUser("ru");
+        var daysToAddForFirstMeeting = -1;
+        var firstMeetingDate = DataGenerator.generateDate(daysToAddForFirstMeeting);
+        var daysToAddForSecondMeeting = 7;
+        var secondMeetingDate = DataGenerator.generateDate(daysToAddForSecondMeeting);
+        $("[data-test-id='city'] input").val(validUser.getCity());
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
+        $("[data-test-id='date'] input").val(firstMeetingDate);
+        $("[data-test-id='name'] input").val(validUser.getName());
+        $("[data-test-id='phone'] input").val(validUser.getPhone());
+        $("[data-test-id='agreement']").click();
+        $(".button").click();
+        $("[data-test-id='date'] .input__inner .input__sub")
+                .shouldBe(visible)
+                .shouldHave(text("Заказ на выбранную дату невозможен"));
+    }
+
+    @Test
+    @DisplayName("Should stop because of out of name")
+    void shouldNotSendFormWithoutName() {
+        var validUser = DataGenerator.Registration.generateUser("ru");
+        var daysToAddForFirstMeeting = 4;
+        var firstMeetingDate = DataGenerator.generateDate(daysToAddForFirstMeeting);
+        var daysToAddForSecondMeeting = 7;
+        var secondMeetingDate = DataGenerator.generateDate(daysToAddForSecondMeeting);
+        $("[data-test-id='city'] input").val(validUser.getCity());
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
+        $("[data-test-id='date'] input").val(firstMeetingDate);
+        $("[data-test-id='phone'] input").val(validUser.getPhone());
+        $("[data-test-id='agreement']").click();
+        $(".button").click();
+        $("[data-test-id='name'] .input__inner .input__sub")
+                .shouldBe(visible)
+                .shouldHave(text("Поле обязательно для заполнения"));
+    }
+
+    @Test
+    @DisplayName("Should stop because of out of number")
+    void shouldNotSendFormWithoutPhone() {
+        var validUser = DataGenerator.Registration.generateUser("ru");
+        var daysToAddForFirstMeeting = 4;
+        var firstMeetingDate = DataGenerator.generateDate(daysToAddForFirstMeeting);
+        var daysToAddForSecondMeeting = 7;
+        var secondMeetingDate = DataGenerator.generateDate(daysToAddForSecondMeeting);
+        $("[data-test-id='city'] input").val(validUser.getCity());
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
+        $("[data-test-id='date'] input").val(firstMeetingDate);
+        $("[data-test-id='name'] input").val(validUser.getName());
+        $("[data-test-id='agreement']").click();
+        $(".button").click();
+        $("[data-test-id='phone'] .input__inner .input__sub")
+                .shouldBe(visible)
+                .shouldHave(text("Поле обязательно для заполнения"));
+    }
+
+    @Test
+    @DisplayName("Should stop because of out of checkbox")
+    void shouldNotSendFormWithoutCheckBox() {
+        var validUser = DataGenerator.Registration.generateUser("ru");
+        var daysToAddForFirstMeeting = 4;
+        var firstMeetingDate = DataGenerator.generateDate(daysToAddForFirstMeeting);
+        var daysToAddForSecondMeeting = 7;
+        var secondMeetingDate = DataGenerator.generateDate(daysToAddForSecondMeeting);
+        $("[data-test-id='city'] input").val(validUser.getCity());
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
+        $("[data-test-id='name'] input").val(validUser.getName());
+        $("[data-test-id='date'] input").val(firstMeetingDate);
+        $(".button").click();
+        $("[data-test-id='agreement'] .checkbox__text")
+                .shouldBe(visible)
+                .shouldHave(text("Я соглашаюсь с условиями обработки и использования моих персональных данных"),
+                        Condition.cssValue("user-select", "none"));
+    }
+}
